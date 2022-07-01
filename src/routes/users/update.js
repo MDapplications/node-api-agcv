7ca37ebd0e654274e7b251344c02fa43f6e48f8c
MDapplications <michel.drault@gmail.com>
2022-07-01 19:32:28 +0200

    1.0.1 - MaJ : update users

--- src/routes/users/update.js
+++ src/routes/users/update.js
@@ -8,15 +8,44 @@ module.exports = (app) => {
     app.put('/api/users/:id', auth, (req, res) => {
 
         const id = req.params.id
-        const password = req.body.password //recupération du password pour hashage
 
-        bcrypt.hash(password, 10)
-        .then(hash => {
-            req.body.password = hash //hashage du mot de passe avant update
-            
+        if (req.body.password) {
+
+            const password = req.body.password //recupération du password pour hashage
+
+            bcrypt.hash(password, 10)
+            .then(hash => {
+                req.body.password = hash //hashage du mot de passe avant update
+                
+                User.update(req.body, {where: {id}})
+                .then(_ => {
+                    return User.findByPk(id) // retourne la promesse au bloque catch suivant en cas d'erreur 500
+                    .then(user => {
+                        //gestion de l'erreur 404
+                        if(user === null) {
+                            const message = `L'utilisateur demandé n'existe pas. Réessayez avec un autre identifiant.`
+                            return res.status(404).json({message})
+                        }
+                        //ressource presente - status : 200
+                        const message = `L'utilisateur id:${user.id} a bien été modifié.`
+                        res.json({message, data: user })
+                    })
+                })
+                .catch(err => {
+                    if (err instanceof ValidationError) {
+                        return res.status(400).json({message: err.message, data: err})
+                    }
+                    const message = `L'utilisateur n'a pas pu être modifié. Réessayez dans quelques instants.`
+                    res.status(500).json({message, data: err})
+                })
+
+            })
+
+        } else {
+
             User.update(req.body, {where: {id}})
             .then(_ => {
-                return User.findByPk(id) // retourne la promesse au bloque catch suivant en cas d'erreur 500
+                return User.findByPk(id)
                 .then(user => {
                     //gestion de l'erreur 404
                     if(user === null) {
@@ -36,6 +65,7 @@ module.exports = (app) => {
                 res.status(500).json({message, data: err})
             })
 
-        })
+        }
+        
     })
 }
